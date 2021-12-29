91777cb7341bfcd2c6da4cd526cf86c9d2ebe347
Sivmeng HUN <sivmengmaths@gmail.com>
2021-12-29 10:56:05 +0700

    28-12-21: basic printf coding

---
+++ c-program/code/note.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hi, my name is Sivmeng.\n");
+  printf("I am a math student at RUPP\n");
+  printf("In my spare time, I love doing math exercise\n");
+  printf("and coding as well.\n");
+  printf("I know python, javascript, c, c++, LaTeX, markdown.\n");
+  printf("And now I am learning a new programming language\n");
+  printf("for doing statistic called R");
+
+  return 0;
+}
